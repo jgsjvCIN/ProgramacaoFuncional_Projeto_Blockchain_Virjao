e91d666959bcf98dc83a9bcf737afc7b4c951e4a
jgsjvCIN <jgsjv@cin.ufpe.br>
2018-06-14 23:24:45 -0300

    Add files via upload

---
+++ server.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Chain
+import           Control.Exception        (SomeException)
+import           Control.Exception.Lifted (handle)
+import           Control.Monad.IO.Class   (liftIO)
+import           Data.Aeson               (Value, encode, object, (.=))
+import           Data.Aeson.Parser        (json)
+import           Data.ByteString          (ByteString)
+import           Data.Conduit             (($$))
+import           Data.Conduit.Attoparsec  (sinkParser)
+import           Network.HTTP.Types       (status200, status400)
+import           Network.Wai              (Application, Response, responseLBS)
+import           Network.Wai.Conduit      (sourceRequestBody)
+import           Network.Wai.Handler.Warp (run)
+
+
+main :: IO ()
+main = run 3000 app
+
+app :: Application
+app req sendResponse = 
