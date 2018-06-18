c7a6148c7b0da09ff980ea510874f55a555d3b90
jgsjvCIN <jgsjv@cin.ufpe.br>
2018-06-18 09:40:33 -0300

    Add files via upload

--- Chain.hs
+++ Chain.hs
@@ -13,12 +13,14 @@ module Chain(
 
     type Chain = [Block]
     
-    data BlockD = BlockD {
-        block :: Block
+    data Info = Info {
+        payer :: String,
+        receiver :: String,
+        value :: Double
     } deriving (Generic, Show)
 
-    instance FromJSON BlockD
-    instance ToJSON BlockD
+    instance FromJSON Info
+    instance ToJSON Info
 
     insert :: Block -> [Block] -> [Block]
     insert b l = b:l
@@ -27,8 +29,13 @@ module Chain(
     createAndInsert trans [] = insert (makeBlock trans 0) [] 
     createAndInsert trans l = insert (makeBlock trans (getHash (head l))) l
 
-    printBlocks :: String -> [Block] -> [BlockD]
-    printBlocks name list = map BlockD (getAll name list)
+    createInfoObj:: Block -> Info
+    createInfoObj ((p , r, v), hash) = Info p r v
+
+    printBlocks :: String -> [Block] -> [Info]
+    printBlocks name list = map createInfoObj (getAll name list)
 
     main :: String -> [Block] -> IO ()
     main name list= B.putStrLn $ encode (printBlocks name list)
+
+    -- [(("San","Al",4.0),-2416514153299201292),(("Al","Bet",3.0),-6711607388473116096),(("Al","San",2.0),8173646634568268900)]
