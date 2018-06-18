ff36bc8fa653311f1c7a49a367bb7beb52b4def7
jgsjvCIN <jgsjv@cin.ufpe.br>
2018-06-18 17:31:58 -0300

    Add files via upload

--- server.hs
+++ server.hs
@@ -5,11 +5,20 @@ import Network.Wai.Handler.Warp (run)
 import Chain
 
 app :: Application
-app :: Application
 app req respond = if ((requestMethod req) == "POST") then
-    createAndInsert trans list
+    let query = queryString req :: [(ByteString, Maybe ByteString)]
+        payerParam = join $ lookup "payer" query :: Maybe ByteString
+        receiverParam = join $ lookup "receiver" query :: Maybe ByteString
+        valueParam = join $ lookup "value" query :: Maybe ByteString
+        listParam = join $ lookup "list" query :: Maybe ByteString
+        postReturn = createAndInsert (payerParam , receiverParam , valueParam) listParam
+    in responseLBS status200 [(hContentType, "application/json")] postReturn
     else if ((requestMethod req) == "GET")
-        (respond $ responseLBS status200 [] "Hello World")
+        let query = queryString req :: [(ByteString, Maybe ByteString)]
+            nameParam = join $ lookup "name" query :: Maybe ByteString
+            listParam = join $ lookup "list" query :: Maybe ByteString
+            getReturn = getInfo nameParam listParam
+        in responseLBS status200 [(hContentType, "application/json")] getReturn
 
 main :: IO ()
 main = do
